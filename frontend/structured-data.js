26a588e899b8a9c3ff2b9a134f38c8e878367fd2
lakopite <trent.clifton8@gmail.com>
2017-09-20 16:30:50 -0700

    structured data ingest complete

--- frontend/structured-data.js
+++ frontend/structured-data.js
@@ -25,8 +25,7 @@ function parseStructuredData(items) {
     "ItemListOrder": "Unordered",
     "itemListElement": _.map(items, function(i,index_) {
       var item_ = {
-        "@type": "ListItem",
-        "additionalType": ["SingleFamilyResidence","Product"],
+        "@type": ["ListItem","SingleFamilyResidence","Product"]
         "position": index_ + 1,
         "url": host + "listing/" + i.slug,
         "name": i.title,
