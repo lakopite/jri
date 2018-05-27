736fb89dea372fc184f56c0b79ae89cc69e32a07
lakopite <trent.clifton8@gmail.com>
2018-05-27 13:57:35 -0700

    sort listings by status, contentful api settings as chalice env vars, elyse name change, brighter background filter

--- backend/jri-listings/app.py
+++ backend/jri-listings/app.py
@@ -1,12 +1,13 @@
 from chalice import Chalice
+import os
 
 import requests,simplejson,datetime,uuid
 
 app = Chalice(app_name='jri-listings')
 
-access_token = "<INSERT HERE>"
-space_id = "<INSERT HERE>"
-url = "https://cdn.contentful.com/spaces/"+space_id+"/entries?access_token="+access_token+"&content_type=listings"
+access_token = os.environ.get('ACCESS_TOKEN')
+space_id = os.environ.get('SPACE_ID')
+url = "https://cdn.contentful.com/spaces/"+space_id+"/entries?access_token="+access_token+"&content_type=listings" if access_token and space_id else None
 
 category_map = {
  "Farm & Land": "farmandland",
@@ -15,6 +16,18 @@ category_map = {
  "Commercial": "commercial",
  "For Lease/Rent": "forleaserent"
 }
+
+def status_sorter(s):
+	return {
+	 "For Sale": 7,
+	 "In Escrow": 6,
+	 "For Lease": 5,
+	 "For Rent": 4,
+	 "Sold": 3,
+	 "Leased": 2,
+	 "Rented": 1,
+	 "Off Market": 0
+	}.get(s,-1)
 
 @app.route('/listings', methods=['GET'], cors=True, api_key_required=True)
 def index():
@@ -28,6 +41,8 @@ def index():
 	 "error": False
 	}
 	try:
+		if not url:
+			raise ValueError("URL not available, please make sure the contentful environment variables are set")
 		r = requests.get(url)
 		payload = r.json()
 		assets = {i['sys']['id']: i["fields"]["file"] for i in payload["includes"]["Asset"]} if "includes" in payload else {}
@@ -37,6 +52,8 @@ def index():
 			i['fields']['downloads'] = [assets[j['sys']['id']] for j in i['fields']['downloads']] if "downloads" in i['fields'] else []
 			if i['fields']['category'] in category_map:
 				result[category_map[i['fields']['category']]].append(i['fields'])
-	except:
+				result[category_map[i['fields']['category']]].sort(key=lambda x: status_sorter(x.get('status','N/A')), reverse=True)
+	except Exception as e:
+		print "ERROR: %s" % str(e)
 		result['error'] = True
 	return simplejson.dumps(result)
